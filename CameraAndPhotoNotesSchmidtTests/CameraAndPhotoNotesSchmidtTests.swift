1172aa5ea5bd2990bc9f259d83bb3a80a84ce80e
Ava Schmidt <ava.schmidt25@d155.org>
2024-12-08 15:46:12 -0800

    Initial Commit

---
+++ CameraAndPhotoNotesSchmidtTests/CameraAndPhotoNotesSchmidtTests.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAndPhotoNotesSchmidtTests.swift
+//  CameraAndPhotoNotesSchmidtTests
+//
+//  Created by AVA SCHMIDT on 12/8/24.
+//
+
+import Testing
+@testable import CameraAndPhotoNotesSchmidt
+
+struct CameraAndPhotoNotesSchmidtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
